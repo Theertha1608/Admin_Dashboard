74ef13fbbe2ca9f814f78b98fbb84c468979110a
theetha1608 <theerthakunjusss@gmail.com>
2024-09-05 14:36:46 +0530

    Navbar completed

--- src/components/NavMessage.jsx
+++ src/components/NavMessage.jsx
@@ -2,10 +2,83 @@ import React from 'react'
 
 function NavMessage() {
   return (
-    <div>
-      NavMessage
-    </div>
-  )
+    <li className="nav-item dropdown">
+      <a className="nav-link nav-icon" href="/about" data-bs-toggle="dropdown">
+        <i className="bi bi-chat-left-text"></i>
+        <span className="badge bg-primary badge-number">4</span>
+      </a>
+
+      <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications">
+        <li className="dropdown-header">
+          You have 4 new notifications
+          <a href="/about">
+            <span className="badge rounded-pill bg-primary p-2 ms-2">
+              View all
+            </span>
+          </a>
+        </li>
+        <li>
+          <hr className="dropdown-divider" />
+        </li>
+
+        <li className="notification-item">
+          <i className="bi bi-exclamation-circle text-warning"></i>
+          <div>
+            <h4>Notification 1</h4>
+            <p>Quae dolorem earum veritatis oditseno</p>
+            <p>30 min ago</p>
+          </div>
+        </li>
+
+        <li>
+          <hr className="dropdown-divider" />
+        </li>
+
+        <li className="notification-item">
+          <i className="bi bi-x-circle text-danger"></i>
+          <div>
+            <h4>Notification 2</h4>
+            <p>Quae dolorem earum veritatis oditseno</p>
+            <p>1 hr ago</p>
+          </div>
+        </li>
+
+        <li>
+          <hr className="dropdown-divider" />
+        </li>
+
+        <li className="notification-item">
+          <i className="bi bi-check-circle text-success"></i>
+          <div>
+            <h4>Notification 3</h4>
+            <p>Quae dolorem earum veritatis oditseno</p>
+            <p>2 hr ago</p>
+          </div>
+        </li>
+
+        <li>
+          <hr className="dropdown-divider" />
+        </li>
+
+        <li className="notification-item">
+          <i className="bi bi-info-circle text-primary"></i>
+          <div>
+            <h4>Notification 4</h4>
+            <p>Quae dolorem earum veritatis oditseno</p>
+            <p>2 hr ago</p>
+          </div>
+        </li>
+
+        <li>
+          <hr className="dropdown-divider" />
+        </li>
+
+        <li className="dropdown-footer">
+          <a href="/about">Show all notifications</a>
+        </li>
+      </ul>
+    </li>
+  );
 }
 
 export default NavMessage
