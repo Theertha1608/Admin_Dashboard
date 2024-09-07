866787c289a7043c7309a6439df2802f8f810046
theetha1608 <theerthakunjusss@gmail.com>
2024-09-07 11:27:36 +0530

    Table fixed

--- src/components/RecentSales.jsx
+++ src/components/RecentSales.jsx
@@ -1,29 +1,27 @@
-import React, { useState } from 'react'
-import './recentSales.css'
-import RecentSalesTable from './RecentSalesTable'
-import CardFilter from './CardFilter'
-
+import React, { useEffect, useState } from 'react';
+import RecentSalesTable from './RecentSalesTable';
 
 function RecentSales() {
+  const [sales, setSales] = useState([]);
 
-  const [items, setItems] = useState([]);
-  const [filter, setFilter] = useState('Today');
-  const handleFilterChange = filter => {
-      setFilter(filter);
+  const fetchSalesData = () => {
+    fetch('http://localhost:5000/recentsales')
+      .then((res) => res.json())
+      .then((data) => {
+        setSales(data);
+      })
+      .catch((e) => console.log(e.message));
   };
 
+  useEffect(() => {
+    fetchSalesData();  
+  }, []);
+
   return (
-    <div className='card recent-sales overflow-auto'>
-    <CardFilter filterChange={handleFilterChange} />
-    
-    <div className='card-body'>
-        <h5 className='card-title'>
-            Recent Sales <span> | {filter}</span>
-        </h5>
-        <RecentSalesTable itmes={items} />
+    <div>
+      <RecentSalesTable items={sales} />
     </div>
-</div>
-  )
+  );
 }
 
-export default RecentSales
+export default RecentSales;
